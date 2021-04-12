7b3d8d1c55f143ee7b9befd4454758b92acffc5e
jeandar11 <77243427+jeandar11@users.noreply.github.com>
2021-04-12 14:33:39 -0400

    File structure cleanup

--- src/App.js
+++ src/App.js
@@ -8,28 +8,26 @@ import DailyForecast from "./DailyForecast.js";
 export default function App() {
   return (
     <div className="App">
-      <div className="container">
-        <div id="main-app">
-          <Search />
-          <Weather />
-          <HourlyForecast />
-          <DailyForecast />
-        </div>
-        <small id="github-link">
-          <a
-            href="https://github.com/jeandar11/shecodes-react-weather-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Open-source code{" "}
-          </a>
-          by Jeanne Darcel thanks to{" "}
-          <a href="https://shecodes.io" target="_blank" rel="noreferrer">
-            SheCodes
-          </a>{" "}
-          👩‍💻
-        </small>
+      <div id="main-app">
+        <Search />
+        <Weather />
+        <HourlyForecast />
+        <DailyForecast />
       </div>
+      <small id="github-link">
+        <a
+          href="https://github.com/jeandar11/shecodes-react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code{" "}
+        </a>
+        by Jeanne Darcel thanks to{" "}
+        <a href="https://shecodes.io" target="_blank" rel="noreferrer">
+          SheCodes
+        </a>{" "}
+        👩‍💻
+      </small>
     </div>
   );
 }
