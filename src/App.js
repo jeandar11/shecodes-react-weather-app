92e1d5285690b5d6e0e3bd0debd347c4e988ad69
jeandar11 <77243427+jeandar11@users.noreply.github.com>
2021-04-12 14:48:58 -0400

    Bootstrap update

--- src/App.js
+++ src/App.js
@@ -8,26 +8,28 @@ import DailyForecast from "./DailyForecast.js";
 export default function App() {
   return (
     <div className="App">
-      <div id="main-app">
-        <Search />
-        <Weather />
-        <HourlyForecast />
-        <DailyForecast />
+      <div className="container">
+        <div id="main-app">
+          <Search />
+          <Weather />
+          <HourlyForecast />
+          <DailyForecast />
+        </div>
+        <small id="github-link">
+          <a
+            href="https://github.com/jeandar11/shecodes-react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code{" "}
+          </a>
+          by Jeanne Darcel thanks to{" "}
+          <a href="https://shecodes.io" target="_blank" rel="noreferrer">
+            SheCodes
+          </a>{" "}
+          👩‍💻
+        </small>
       </div>
-      <small id="github-link">
-        <a
-          href="https://github.com/jeandar11/shecodes-react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code{" "}
-        </a>
-        by Jeanne Darcel thanks to{" "}
-        <a href="https://shecodes.io" target="_blank" rel="noreferrer">
-          SheCodes
-        </a>{" "}
-        👩‍💻
-      </small>
     </div>
   );
 }
