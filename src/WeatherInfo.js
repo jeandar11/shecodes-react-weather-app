c3609d7cc8284debfb0796330412699191be6461
jeandar11 <77243427+jeandar11@users.noreply.github.com>
2021-04-14 11:00:19 -0400

    Code cleanup

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -11,18 +11,16 @@ export default function WeatherInfo(props) {
       <div className="row">
         <div className="col-sm-6">
           <header>
-            <h1 id="city">{props.data.name}</h1>
-            <h2 id="local-date">
+            <h1>{props.data.name}</h1>
+            <h2>
               <FormattedDate date={props.data.date} />
             </h2>
-            <h2 id="local-time">
+            <h2>
               <FormattedTime time={props.data.time} />
             </h2>
-            <h3 id="weather-description">{props.data.description}</h3>
-            <h4 id="humidity">Humidity: {props.data.humidity}%</h4>
-            <h4 id="wind-speed">
-              Wind Speed: {Math.round(props.data.wind * 3.6)} km/h
-            </h4>
+            <h3>{props.data.description}</h3>
+            <h4>Humidity: {props.data.humidity}%</h4>
+            <h4>Wind Speed: {Math.round(props.data.wind * 3.6)} km/h</h4>
           </header>
         </div>
         <div className="col-sm-6">
